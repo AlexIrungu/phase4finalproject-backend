937b90e9055be8830de8fb98a14a4962718c957e
AlexIrungu <aleaxmuiruri@gmail.com>
2023-03-29 11:11:39 +0300

    configured controllers and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
     def create
-        user = User.find_or_create_by(username: params[:username])
-        #set the class variable when a user signs in on the front end
-        User.set_current_user(user.id)
-
-        render json: user 
-        #, only: [:id, :username], include: :[:notes, :books]
+        if user.valid?
+            session[:user_id] = user.id
+            render json: user, status: :created
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
      def index
@@ -21,11 +21,26 @@ class UsersController < ApplicationController
 
     end
     def update
-        user_book = UserBook.find_by(user_id: params[:id], book_id: params[:book_id])
-        if user_book.destroy
-            book = Book.find(params[:book_id])
-            render json: book
-        else
-            render json: {error: "Error removing book from user's collection"}
+        # user_book = UserBook.find_by(user_id: params[:id], book_id: params[:book_id])
+        # if user_book.destroy
+        #     book = Book.find(params[:book_id])
+        #     render json: book
+        # else
+        #     render json: {error: "Error removing book from user's collection"}
+
+        user = @current_user
+        user.update(user_params)
+        render json: user
+    end
+
+    def destroy
+        @current_user.destroy 
+        head :no_content
+    end
+
+    private 
+
+    def user_params
+        params.permit(:email, :first_name, :last_name,  :password_digest)
     end
 end
